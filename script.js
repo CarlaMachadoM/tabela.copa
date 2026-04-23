e06bc362d5034858e619d8161276cda3966a458b
Carla <carlamasm@gmail.com>
2026-04-23 18:48:03 -0300

    memoria resolvida

--- script.js
+++ script.js
@@ -107,13 +107,24 @@ function load(){
   const data = localStorage.getItem("matches");
 
   if(data){
-    matches = JSON.parse(data); // 🔥 CORREÇÃO PRINCIPAL
+    const saved = JSON.parse(data);
+
+    matches = JSON.parse(JSON.stringify(DEFAULT_MATCHES));
+
+    Object.keys(saved).forEach(group=>{
+      saved[group].forEach((game,i)=>{
+        if(matches[group] && matches[group][i]){
+          matches[group][i].g1 = game.g1;
+          matches[group][i].g2 = game.g2;
+        }
+      });
+    });
+
   } else {
     matches = JSON.parse(JSON.stringify(DEFAULT_MATCHES));
     save();
   }
 }
-
 
 function clearGroup(group){
   matches[group].forEach(g=>{
@@ -124,41 +135,7 @@ function clearGroup(group){
   render();
 }
 
-function createGroup(name,games){
-
-  games.sort((a,b)=>{
-    return new Date(`2026-${a.date.split("/").reverse().join("-")}T${a.time}`) -
-           new Date(`2026-${b.date.split("/").reverse().join("-")}T${b.time}`);
-  });
-
-  const div = document.createElement("div");
-  div.className="card";
-
-  let html = `<h3>Grupo ${name}</h3>`;
-
-  games.forEach((g,i)=>{
-    html += `
-    <div class="match">
-      ${g.date} ${g.time}<br>
-      ${g.t1}
-      <input value="${g.g1 ?? ''}" onchange="update('${name}',${i},0,this.value)">
-      x
-      <input value="${g.g2 ?? ''}" onchange="update('${name}',${i},1,this.value)">
-      ${g.t2}
-    </div>`;
-  });
-
-  html += `
-  <button class="clear-btn" onclick="clearGroup('${name}')">Limpar</button>
-  <div class="standings" id="stand-${name}"></div>`;
-
-  div.innerHTML = html;
-  return div;
-}
-
-
 function update(group,i,side,val){
-
   if(val===""){
     if(side===0) delete matches[group][i].g1;
     else delete matches[group][i].g2;
@@ -166,15 +143,12 @@ function update(group,i,side,val){
     if(side===0) matches[group][i].g1 = parseInt(val);
     else matches[group][i].g2 = parseInt(val);
   }
-
-  save(); // 🔥 ESSENCIAL
+  save();
   render();
 }
 
-
 function calc(group){
   const table = {};
-
   matches[group].forEach(g=>{
     [g.t1,g.t2].forEach(t=>{
       if(!table[t]) table[t]={pts:0,gf:0,ga:0};
@@ -200,12 +174,35 @@ function calc(group){
     .sort((a,b)=>b.pts-a.pts || b.sg-a.sg || b.gf-a.gf);
 }
 
+function createGroup(name,games){
+  const div=document.createElement("div");
+  div.className="card";
+
+  let html=`<h3>Grupo ${name}</h3>`;
+
+  games.forEach((g,i)=>{
+    html+=`
+    <div class="match">
+      ${g.date} ${g.time}<br>
+      ${g.t1}
+      <input value="${g.g1 ?? ''}" onchange="update('${name}',${i},0,this.value)">
+      x
+      <input value="${g.g2 ?? ''}" onchange="update('${name}',${i},1,this.value)">
+      ${g.t2}
+    </div>`;
+  });
+
+  html+=`<button class="clear-btn" onclick="clearGroup('${name}')">Limpar</button>
+  <div class="standings" id="stand-${name}"></div>`;
+
+  div.innerHTML=html;
+  return div;
+}
 
 function generateKnockout(){
   let classificados=[];
-
   Object.keys(matches).forEach(g=>{
-    const res = calc(g);
+    const res=calc(g);
     if(res.length>=2){
       classificados.push(res[0].time,res[1].time);
     }
@@ -216,11 +213,10 @@ function generateKnockout(){
 
   for(let i=0;i<classificados.length;i+=2){
     if(classificados[i+1]){
-      div.innerHTML += `<div>${classificados[i]} x ${classificados[i+1]}</div>`;
+      div.innerHTML+=`<div>${classificados[i]} x ${classificados[i+1]}</div>`;
     }
   }
 }
-
 
 function render(){
   const app=document.getElementById("app");
@@ -229,14 +225,13 @@ function render(){
   Object.entries(matches).forEach(([g,games])=>{
     app.appendChild(createGroup(g,games));
 
-    const res = calc(g);
+    const res=calc(g);
     document.getElementById("stand-"+g).innerHTML =
       res.map(t=>`${t.time} - ${t.pts} pts`).join("<br>");
   });
 
   generateKnockout();
 }
-
 
 function init(){
   load();
