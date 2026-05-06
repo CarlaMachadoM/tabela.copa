1e088b6a85d73bb185c3c58691a93036d199f9d8
Carla Machado <160264781+CarlaMachadoM@users.noreply.github.com>
2026-05-06 13:18:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,131 +1,33 @@
 const DEFAULT_MATCHES = {
-"A":[
-{t1:"México",t2:"África do Sul",date:"11/06",time:"16:00"},
-{t1:"Coreia do Sul",t2:"República Tcheca",date:"11/06",time:"23:00"},
-{t1:"República Tcheca",t2:"África do Sul",date:"18/06",time:"13:00"},
-{t1:"México",t2:"Coreia do Sul",date:"18/06",time:"22:00"},
-{t1:"República Tcheca",t2:"México",date:"24/06",time:"22:00"},
-{t1:"África do Sul",t2:"Coreia do Sul",date:"24/06",time:"22:00"}
-],
-"B":[
-{t1:"Canadá",t2:"Bósnia e Herzegovina",date:"12/06",time:"16:00"},
-{t1:"Catar",t2:"Suíça",date:"13/06",time:"16:00"},
-{t1:"Suíça",t2:"Canadá",date:"18/06",time:"16:00"},
-{t1:"Canadá",t2:"Catar",date:"18/06",time:"19:00"},
-{t1:"Suíça",t2:"Canadá",date:"24/06",time:"16:00"},
-{t1:"Bósnia e Herzegovina",t2:"Catar",date:"24/06",time:"16:00"}
-],
-"C":[
-{t1:"Brasil",t2:"Marrocos",date:"13/06",time:"19:00"},
-{t1:"Haiti",t2:"Escócia",date:"13/06",time:"22:00"},
-{t1:"Escócia",t2:"Marrocos",date:"19/06",time:"19:00"},
-{t1:"Brasil",t2:"Haiti",date:"19/06",time:"21:30"},
-{t1:"Escócia",t2:"Brasil",date:"24/06",time:"19:00"},
-{t1:"Marrocos",t2:"Haiti",date:"24/06",time:"19:00"}
-],
-"D":[
-{t1:"Estados Unidos",t2:"Paraguai",date:"12/06",time:"22:00"},
-{t1:"Austrália",t2:"Turquia",date:"13/06",time:"01:00"},
-{t1:"Turquia",t2:"Paraguai",date:"19/06",time:"01:00"},
-{t1:"Estados Unidos",t2:"Austrália",date:"19/06",time:"16:00"},
-{t1:"Turquia",t2:"Estados Unidos",date:"25/06",time:"23:00"},
-{t1:"Paraguai",t2:"Austrália",date:"25/06",time:"23:00"}
-],
-"E":[
-{t1:"Alemanha",t2:"Curaçao",date:"14/06",time:"14:00"},
-{t1:"Costa do Marfim",t2:"Equador",date:"14/06",time:"20:00"},
-{t1:"Alemanha",t2:"Costa do Marfim",date:"20/06",time:"17:00"},
-{t1:"Equador",t2:"Curaçao",date:"20/06",time:"21:00"},
-{t1:"Equador",t2:"Alemanha",date:"25/06",time:"17:00"},
-{t1:"Curaçao",t2:"Costa do Marfim",date:"25/06",time:"17:00"}
-],
-"F":[
-{t1:"Países Baixos",t2:"Japão",date:"14/06",time:"17:00"},
-{t1:"Suécia",t2:"Tunísia",date:"14/06",time:"23:00"},
-{t1:"Países Baixos",t2:"Suécia",date:"20/06",time:"14:00"},
-{t1:"Tunísia",t2:"Japão",date:"20/06",time:"01:00"},
-{t1:"Japão",t2:"Suécia",date:"25/06",time:"20:00"},
-{t1:"Tunísia",t2:"Países Baixos",date:"25/06",time:"20:00"}
-],
-"G":[
-{t1:"Bélgica",t2:"Egito",date:"15/06",time:"16:00"},
-{t1:"Irã",t2:"Nova Zelândia",date:"15/06",time:"22:00"},
-{t1:"Bélgica",t2:"Irã",date:"21/06",time:"16:00"},
-{t1:"Nova Zelândia",t2:"Egito",date:"21/06",time:"22:00"},
-{t1:"Egito",t2:"Irã",date:"26/06",time:"00:00"},
-{t1:"Nova Zelândia",t2:"Bélgica",date:"26/06",time:"00:00"}
-],
-"H":[
-{t1:"Espanha",t2:"Cabo Verde",date:"15/06",time:"13:00"},
-{t1:"Arábia Saudita",t2:"Uruguai",date:"15/06",time:"19:00"},
-{t1:"Espanha",t2:"Arábia Saudita",date:"21/06",time:"13:00"},
-{t1:"Uruguai",t2:"Cabo Verde",date:"21/06",time:"19:00"},
-{t1:"Cabo Verde",t2:"Arábia Saudita",date:"26/06",time:"21:00"},
-{t1:"Uruguai",t2:"Espanha",date:"26/06",time:"21:00"}
-],
-"I":[
-{t1:"França",t2:"Senegal",date:"16/06",time:"16:00"},
-{t1:"Iraque",t2:"Noruega",date:"16/06",time:"19:00"},
-{t1:"França",t2:"Iraque",date:"22/06",time:"18:00"},
-{t1:"Noruega",t2:"Senegal",date:"22/06",time:"21:00"},
-{t1:"Noruega",t2:"França",date:"26/06",time:"16:00"},
-{t1:"Senegal",t2:"Iraque",date:"26/06",time:"16:00"}
-],
-"J":[
-{t1:"Argentina",t2:"Argélia",date:"16/06",time:"22:00"},
-{t1:"Áustria",t2:"Jordânia",date:"17/06",time:"01:00"},
-{t1:"Argentina",t2:"Áustria",date:"22/06",time:"14:00"},
-{t1:"Jordânia",t2:"Argélia",date:"22/06",time:"00:00"},
-{t1:"Argélia",t2:"Áustria",date:"27/06",time:"23:00"},
-{t1:"Jordânia",t2:"Argentina",date:"27/06",time:"23:00"}
-],
-"K":[
-{t1:"Portugal",t2:"RD Congo",date:"17/06",time:"14:00"},
-{t1:"Uzbequistão",t2:"Colômbia",date:"17/06",time:"23:00"},
-{t1:"Portugal",t2:"Uzbequistão",date:"23/06",time:"14:00"},
-{t1:"Colômbia",t2:"RD Congo",date:"23/06",time:"23:00"},
-{t1:"Colômbia",t2:"Portugal",date:"27/06",time:"20:30"},
-{t1:"RD Congo",t2:"Uzbequistão",date:"27/06",time:"20:30"}
-],
-"L":[
-{t1:"Inglaterra",t2:"Croácia",date:"17/06",time:"17:00"},
-{t1:"Gana",t2:"Panamá",date:"17/06",time:"20:00"},
-{t1:"Inglaterra",t2:"Gana",date:"23/06",time:"17:00"},
-{t1:"Panamá",t2:"Croácia",date:"23/06",time:"20:00"},
-{t1:"Panamá",t2:"Inglaterra",date:"27/06",time:"18:00"},
-{t1:"Croácia",t2:"Gana",date:"27/06",time:"19:00"}
-]
+A:[{t1:"México",t2:"África do Sul"},{t1:"Coreia do Sul",t2:"República Tcheca"}],
+B:[{t1:"Canadá",t2:"Bósnia e Herzegovina"},{t1:"Catar",t2:"Suíça"}],
+C:[{t1:"Brasil",t2:"Marrocos"},{t1:"Haiti",t2:"Escócia"}],
+D:[{t1:"Estados Unidos",t2:"Paraguai"},{t1:"Austrália",t2:"Turquia"}],
+E:[{t1:"Alemanha",t2:"Curaçao"}],
+F:[{t1:"Países Baixos",t2:"Japão"}],
+G:[{t1:"Bélgica",t2:"Egito"}],
+H:[{t1:"Espanha",t2:"Cabo Verde"}],
+I:[{t1:"França",t2:"Senegal"}],
+J:[{t1:"Argentina",t2:"Argélia"}],
+K:[{t1:"Portugal",t2:"Repescagem 1"}],
+L:[{t1:"Inglaterra",t2:"Croácia"}]
 };
 
 let matches = {};
+let knockout = {};
 
+// ================= STORAGE =================
 function save(){
   localStorage.setItem("matches", JSON.stringify(matches));
+  localStorage.setItem("knockout", JSON.stringify(knockout));
 }
 
 function load(){
-  const data = localStorage.getItem("matches");
-
-  if(data){
-    const saved = JSON.parse(data);
-
-    matches = JSON.parse(JSON.stringify(DEFAULT_MATCHES));
-
-    Object.keys(saved).forEach(group=>{
-      saved[group].forEach((game,i)=>{
-        if(matches[group] && matches[group][i]){
-          matches[group][i].g1 = game.g1;
-          matches[group][i].g2 = game.g2;
-        }
-      });
-    });
-
-  } else {
-    matches = JSON.parse(JSON.stringify(DEFAULT_MATCHES));
-    save();
-  }
+  matches = JSON.parse(localStorage.getItem("matches")) || JSON.parse(JSON.stringify(DEFAULT_MATCHES));
+  knockout = JSON.parse(localStorage.getItem("knockout")) || {};
 }
 
+// ================= LIMPAR =================
 function clearGroup(group){
   matches[group].forEach(g=>{
     delete g.g1;
@@ -135,20 +37,32 @@ function clearGroup(group){
   render();
 }
 
-function update(group,i,side,val){
-  if(val===""){
-    if(side===0) delete matches[group][i].g1;
-    else delete matches[group][i].g2;
-  } else {
-    if(side===0) matches[group][i].g1 = parseInt(val);
-    else matches[group][i].g2 = parseInt(val);
-  }
+function clearPhase(phase){
+  knockout[phase]?.forEach(g=>{
+    delete g.g1;
+    delete g.g2;
+  });
   save();
   render();
 }
 
+// ================= UPDATE =================
+function update(group,i,side,val){
+  matches[group][i][side===0?"g1":"g2"]=val===""?null:parseInt(val);
+  save();
+  render();
+}
+
+function updateKO(phase,i,side,val){
+  knockout[phase][i][side===0?"g1":"g2"]=val===""?null:parseInt(val);
+  save();
+  render();
+}
+
+// ================= CLASSIFICAÇÃO =================
 function calc(group){
-  const table = {};
+  const table={};
+
   matches[group].forEach(g=>{
     [g.t1,g.t2].forEach(t=>{
       if(!table[t]) table[t]={pts:0,gf:0,ga:0};
@@ -162,7 +76,7 @@ function calc(group){
 
       if(g.g1>g.g2) table[g.t1].pts+=3;
       else if(g.g2>g.g1) table[g.t2].pts+=3;
-      else {
+      else{
         table[g.t1].pts++;
         table[g.t2].pts++;
       }
@@ -171,9 +85,32 @@ function calc(group){
 
   return Object.entries(table)
     .map(([t,v])=>({...v,time:t,sg:v.gf-v.ga}))
-    .sort((a,b)=>b.pts-a.pts || b.sg-a.sg || b.gf-a.gf);
+    .sort((a,b)=>b.pts-a.pts||b.sg-a.sg||b.gf-a.gf);
 }
 
+function getWinner(game){
+  if(game?.g1==null||game?.g2==null) return "---";
+  return game.g1>game.g2 ? game.t1 : game.t2;
+}
+
+function getLoser(game){
+  if(game?.g1==null||game?.g2==null) return "---";
+  return game.g1<game.g2 ? game.t1 : game.t2;
+}
+
+// ================= MELHORES TERCEIROS =================
+function getBestThirds(){
+  let thirds=[];
+
+  Object.keys(matches).forEach(g=>{
+    const res=calc(g);
+    if(res[2]) thirds.push(res[2]);
+  });
+
+  return thirds.sort((a,b)=>b.pts-a.pts||b.sg-a.sg||b.gf-a.gf).slice(0,8);
+}
+
+// ================= GRUPOS =================
 function createGroup(name,games){
   const div=document.createElement("div");
   div.className="card";
@@ -183,41 +120,111 @@ function createGroup(name,games){
   games.forEach((g,i)=>{
     html+=`
     <div class="match">
-      ${g.date} ${g.time}<br>
       ${g.t1}
-      <input value="${g.g1 ?? ''}" onchange="update('${name}',${i},0,this.value)">
+      <input value="${g.g1??''}" onchange="update('${name}',${i},0,this.value)">
       x
-      <input value="${g.g2 ?? ''}" onchange="update('${name}',${i},1,this.value)">
+      <input value="${g.g2??''}" onchange="update('${name}',${i},1,this.value)">
       ${g.t2}
     </div>`;
   });
 
-  html+=`<button class="clear-btn" onclick="clearGroup('${name}')">Limpar</button>
+  html+=`<button onclick="clearGroup('${name}')">Limpar</button>
   <div class="standings" id="stand-${name}"></div>`;
 
   div.innerHTML=html;
   return div;
 }
 
-function generateKnockout(){
-  let classificados=[];
-  Object.keys(matches).forEach(g=>{
-    const res=calc(g);
-    if(res.length>=2){
-      classificados.push(res[0].time,res[1].time);
-    }
-  });
+// ================= FASES =================
+function buildPhase(name,teams){
+  if(!knockout[name]){
+    knockout[name]=[];
 
-  const div=document.getElementById("knockout");
-  div.innerHTML="";
-
-  for(let i=0;i<classificados.length;i+=2){
-    if(classificados[i+1]){
-      div.innerHTML+=`<div>${classificados[i]} x ${classificados[i+1]}</div>`;
+    for(let i=0;i<teams.length;i+=2){
+      knockout[name].push({
+        t1:teams[i],
+        t2:teams[i+1]
+      });
     }
   }
 }
 
+function createPhase(name,title){
+  const container=document.getElementById("phase32");
+
+  const h=document.createElement("h2");
+  h.innerText=title;
+  container.appendChild(h);
+
+  knockout[name].forEach((g,i)=>{
+    const div=document.createElement("div");
+    div.className="card";
+
+    div.innerHTML=`
+      <h3>${title} ${i+1}</h3>
+      <div class="match">
+        ${g.t1}
+        <input value="${g.g1??''}" onchange="updateKO('${name}',${i},0,this.value)">
+        x
+        <input value="${g.g2??''}" onchange="updateKO('${name}',${i},1,this.value)">
+        ${g.t2}
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+
+  const btn=document.createElement("button");
+  btn.innerText="Limpar";
+  btn.onclick=()=>clearPhase(name);
+  container.appendChild(btn);
+}
+
+// ================= KNOCKOUT =================
+function generateKnockout(){
+  const standings={};
+  Object.keys(matches).forEach(g=>standings[g]=calc(g));
+
+  const thirds=getBestThirds();
+
+  const phase32Teams=[
+    standings.A?.[1]?.time, standings.B?.[1]?.time,
+    standings.E?.[0]?.time, thirds[0]?.time,
+    standings.F?.[0]?.time, standings.C?.[1]?.time,
+    standings.C?.[0]?.time, standings.F?.[1]?.time,
+    standings.I?.[0]?.time, thirds[1]?.time,
+    standings.E?.[1]?.time, standings.I?.[1]?.time,
+    standings.A?.[0]?.time, thirds[2]?.time,
+    standings.L?.[0]?.time, thirds[3]?.time,
+    standings.D?.[0]?.time, thirds[4]?.time,
+    standings.G?.[0]?.time, thirds[5]?.time,
+    standings.K?.[1]?.time, standings.L?.[1]?.time,
+    standings.H?.[0]?.time, standings.J?.[1]?.time,
+    standings.B?.[0]?.time, thirds[6]?.time,
+    standings.J?.[0]?.time, standings.H?.[1]?.time,
+    standings.K?.[0]?.time, thirds[7]?.time,
+    standings.D?.[1]?.time, standings.G?.[1]?.time
+  ];
+
+  buildPhase("32",phase32Teams);
+  buildPhase("16",knockout["32"].map(getWinner));
+  buildPhase("8",knockout["16"].map(getWinner));
+  buildPhase("4",knockout["8"].map(getWinner));
+  buildPhase("3",[...knockout["4"].map(getLoser)]);
+  buildPhase("final",knockout["4"].map(getWinner));
+
+  const div=document.getElementById("phase32");
+  div.innerHTML="";
+
+  createPhase("32","32-avos");
+  createPhase("16","Oitavas");
+  createPhase("8","Quartas");
+  createPhase("4","Semifinais");
+  createPhase("3","3º Lugar");
+  createPhase("final","Final");
+}
+
+// ================= RENDER =================
 function render(){
   const app=document.getElementById("app");
   app.innerHTML="";
@@ -226,13 +233,14 @@ function render(){
     app.appendChild(createGroup(g,games));
 
     const res=calc(g);
-    document.getElementById("stand-"+g).innerHTML =
-      res.map(t=>`${t.time} - ${t.pts} pts`).join("<br>");
+    document.getElementById("stand-"+g).innerHTML=
+      res.map((t,i)=>`${i+1}º ${t.time} - ${t.pts} pts`).join("<br>");
   });
 
   generateKnockout();
 }
 
+// ================= INIT =================
 function init(){
   load();
   render();
